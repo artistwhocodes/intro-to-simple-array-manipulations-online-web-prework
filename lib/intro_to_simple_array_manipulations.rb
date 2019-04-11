38fcb8cfde295e9f32f06125599031d761da7d72
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-11 01:37:17 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,54 @@
+def using_push( array , string)
+  array.push(string)
+end
+
+def using_unshift( array , string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  new_array = array.pop
+  return new_array
+end
+
+def pop_with_args(array)
+  new_array = array.pop(2)
+  return new_array
+end
+
+def using_shift(array)
+  new_array = array.shift
+  return new_array
+end
+
+def shift_with_args(array)
+  new_array = array.shift(2)
+  return new_array
+end
+
+def using_concat(array, array_two)
+  array.concat(array_two)
+end
+
+def using_insert(array , element)
+  wtf = array.insert(4 , element)
+  return wtf
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  idk = array.flatten
+  return idk
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
+#learn --fail-fast
